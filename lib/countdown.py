8319870afb4450f000a37165db5bc86981bb2b20
carolinemilz <christine.muriungi@student.moringaschool.com>
2024-03-26 10:07:49 +0300

    completed lab

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,17 @@
 # your code goes here!
+import time
+
+def countdown(i):
+    i = 5
+    while i >0:
+        print (f"{i} SECOND(S)!")
+        i -= 1
+    print('HAPPY NEW YEAR!')    
+
+def countdown_with_sleep(i):
+    i = 5
+    while i >0:
+        print (f"{i} SECOND(S)!")
+        i -= 1
+        time.sleep(1)
+    print('HAPPY NEW YEAR!')    
